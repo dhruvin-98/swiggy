1d8a5ee6ec86e031505979e74e4be6f4e0832777
dhruvin <dhruvinborad1910@gmail.com>
2025-03-13 18:20:22 +0530

    final changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,13 @@
+
+// first div: swiggy ka logo
+import { Link } from "react-router"
+
 export default function Header(){
 
-    return(
+
+    return (
         <header className="bg-[#ff5200] font-serif">
-            <div className="flex justify-between items-center container mx-auto px-10 py-8">
+            <div className="flex justify-between container mx-auto py-8">
                 <img className="w-40 h-12" src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png"></img>
                 <div className=" text-white text-base font-bold flex gap-15 items-center">
                     <a target="_blank" href="https://www.swiggy.com/corporate/">Swiggy Corporate</a>
@@ -11,21 +16,23 @@ export default function Header(){
                     <a className="border border-black bg-black py-3 px-4 rounded-2xl"target="_blank" href="https://www.swiggy.com/corporate/">Sign in</a>
                 </div>
             </div>
+
             <div className="pt-16 pb-8 relative">
                  <img className="h-110 w-60 absolute top-0 left-0" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Veggies_new.png"></img>
                  <img className="h-110 w-60 absolute top-0 right-0" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Sushi_replace.png"></img>
                 <div className="max-w-[60%] text-5xl text-white font-bold container mx-auto text-center">
                     Order Food and groceries. Discover best restaurants. Swiggy it!
                 </div>
-                <div className="max-w-[60%] container mx-auto flex gap-5 mt-10">
+                <div className="max-w-[70%] container mx-auto flex gap-5 mt-10">
                  <input className="bg-white w-[40%] text-xl px-6 py-4 rounded-2xl" placeholder="Delhi, India"></input>
                  <input className="bg-white w-[55%] text-xl px-6 py-4 rounded-2xl" placeholder="Search for restaurant and items for more"></input>
                 </div>
             </div>
+
             <div className="max-w-[80%] container mx-auto flex">
-                <a href="https://www.swiggy.com/restaurants">
+                <Link to="/restaurant">
                     <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/1/fa01e85b-3057-482d-9523-5289722b1df2_Food4BU.png"></img>
-                </a>
+                </Link>
                 <a href="https://www.swiggy.com/instamart?entryId=1234&entryName=mainTileEntry4&v=1">
                     <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/16/ca34e375-f1bd-4a2e-a3e7-0a20833be83b_IM4BU1.png"></img>
                 </a>
